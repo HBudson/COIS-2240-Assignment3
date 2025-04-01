1538a77141960235dcd0ae902647c9174e607fb7
HBudson <hudsontaylor@trentu.ca>
2025-04-01 14:23:43 -0400

    Refactored capitalization logic in the Vehicle constructor
    The new method, capitalize, will do the same thing as the code above- checking if a string is null or empty, then capitalizing the first character while making all others lowercase if not- to any string we plug in. Calling this method on the make and model when we make a vehicle should improve clarity while accomplishing the same thing.

--- RentalProject/Vehicle.java
+++ RentalProject/Vehicle.java
@@ -12,20 +12,20 @@ public abstract class Vehicle implements Serializable {
     public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
+    	capitalize(make);
+    	capitalize(model);
     	
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
         this.licensePlate = null;
     }
+
+	private void capitalize(String input) {
+		if (input == null || input.isEmpty())
+    		input = null;
+    	else
+    		input = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+	}
 
     public Vehicle() {
         this(null, null, 0);
