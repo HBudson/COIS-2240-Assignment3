901e1f01202274f3f414e9f1fcce7de89f2aebcb
HBudson <hudsontaylor@trentu.ca>
2025-04-03 20:30:39 -0400

    Rent and return testing
    I added new test cases for renting and returning vehicles, to make sure it's working properly. I then had to clear the .ser files, because I wanted to run both that test case and the license plate validation test. Both went off without a hitch. The only thing is that the .ser file didn't recreate itself- for one reason or another, the test customers don't seem to get saved to the .ser file. It's of no serious concern to me, just bringing it up for transparency.

--- RentalProject/RentalSystem.java
+++ RentalProject/RentalSystem.java
@@ -121,7 +121,7 @@ public class RentalSystem {
     	}
     }
 
-    public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
+    public boolean rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.AVAILABLE) {
             vehicle.setStatus(Vehicle.VehicleStatus.RENTED);
             RentalRecord record = new RentalRecord(vehicle, customer, date, amount, "RENT");
@@ -130,13 +130,15 @@ public class RentalSystem {
             System.out.println("Vehicle rented to " + customer.getCustomerName());
             
             saveVehicle(vehicle);
+            return true;
         }
         else {
             System.out.println("Vehicle is not available for renting.");
+            return false;
         }
     }
 
-    public void returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
+    public boolean returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.RENTED) {
             vehicle.setStatus(Vehicle.VehicleStatus.AVAILABLE);
             RentalRecord record = new RentalRecord(vehicle, customer, date, extraFees, "RETURN");
@@ -145,9 +147,11 @@ public class RentalSystem {
             System.out.println("Vehicle returned by " + customer.getCustomerName());
             
             saveVehicle(vehicle);
+            return true;
         }
         else {
             System.out.println("Vehicle is not rented.");
+            return false;
         }
     }    
 
--- RentalProject/VehicleRentalTest.java
+++ RentalProject/VehicleRentalTest.java
@@ -1,5 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 class VehicleRentalTest {
@@ -19,5 +21,28 @@ class VehicleRentalTest {
 		assertThrows(IllegalArgumentException.class, () -> testV.setLicensePlate(null));
 		assertThrows(IllegalArgumentException.class, () -> testV.setLicensePlate("AAA1000"));
 		assertThrows(IllegalArgumentException.class, () -> testV.setLicensePlate("ZZZ99"));
+	}
+	
+	@Test
+	public void testRentAndReturnVehicle() {
+		Vehicle testV = new Truck("Dodge", "Charger", 2016, 5);
+		Customer testC = new Customer(12345, "Bob");
+		testV.setLicensePlate("ABC123");
+		assertTrue("ABC123".equals(testV.getLicensePlate()));
+		assertTrue(testV.getStatus().equals(Vehicle.VehicleStatus.AVAILABLE));
+		
+		RentalSystem rentSys = RentalSystem.getInstance();
+		
+		boolean rentResult = rentSys.rentVehicle(testV, testC, LocalDate.now(), 100);
+		assertTrue(rentResult == true);
+		assertTrue(testV.getStatus().equals(Vehicle.VehicleStatus.RENTED));
+		rentResult = rentSys.rentVehicle(testV, testC, LocalDate.now(), 100);
+		assertTrue(rentResult == false);
+		
+		boolean returnResult = rentSys.returnVehicle(testV, testC, LocalDate.now(), 50);
+		assertTrue(returnResult == true);
+		assertTrue(testV.getStatus().equals(Vehicle.VehicleStatus.AVAILABLE));
+		returnResult = rentSys.returnVehicle(testV, testC, LocalDate.now(), 50);
+		assertTrue(returnResult == false);
 	}
 }
